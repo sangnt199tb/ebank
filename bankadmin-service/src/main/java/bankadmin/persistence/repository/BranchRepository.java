989b77c44e889a0aadd7685c871d09c4d1ef6b08
sangnt199tb <71488764+sangnt199tb@users.noreply.github.com>
2026-04-13 19:34:11 +0700

    fix pom admin api gateway

--- bankadmin-service/src/main/java/bankadmin/persistence/repository/BranchRepository.java
+++ bankadmin-service/src/main/java/bankadmin/persistence/repository/BranchRepository.java
@@ -1,8 +1,9 @@
 package bankadmin.persistence.repository;
 
+import bankadmin.persistence.domain.BankEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BranchRepository extends JpaRepository<BankRepository, String> {
+public interface BranchRepository extends JpaRepository<BankEntity, String> {
 }
