0ff1b75d4629cbeba91495bdeac1f4360190b45a
sangnt199tb <71488764+sangnt199tb@users.noreply.github.com>
2026-04-13 19:26:24 +0700

    fix pom admin api gateway

--- bankadmin-service/src/main/java/bankadmin/BankAdminServiceApplication.java
+++ bankadmin-service/src/main/java/bankadmin/BankAdminServiceApplication.java
@@ -3,9 +3,11 @@ package bankadmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "bankadmin.persistence.repository")
 public class BankAdminServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankAdminServiceApplication.class, args);
